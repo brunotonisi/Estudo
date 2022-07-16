0c75d05b9aa8325f07f45f9e53b502d5bc85bfb6
Bruno <brunotonisi@gmail.com>
2022-07-16 10:03:24 -0300

    ïntroducao ao js

---
+++ scripts.js
@@ -0,0 +1,45 @@
+//let weekDay= "sabado";
+
+//if(weekDay === "quarta" || weekDay === "quinta" || weekDay === "sexta" || weekDay === "segunda" || weekDay === "terça"){
+//    console.log("tem que estudar hoje!");
+//}
+//if(weekDay === "sabado" || weekDay=== "domingo"){
+//    console.log("dia de fumar muito");
+//}
+
+//console.log(!(2*2)===4);
+
+//console.log(!0);
+
+//let trafficLight="roxo";
+
+//switch (trafficLight) {
+//  case "vermelho":
+//    console.log("Pare");
+//  break;
+//  case "verde":
+//    console.log("Siga");
+//  break;
+//  case "amarelo":
+//     console.log("Atenção!");
+//    break;
+//   default:
+//      console.log("Você não está dirigindo.");
+
+//}
+
+let estado="teste";
+
+switch (estado) {
+    case "aprovado":
+        console.log("Parabens, clique aqui para fazer sua matrícula.");
+        break;
+    case "lista":
+        console.log("Enviaremos um e-mail caso haja desistência de algum candidato aprovado, aguarde.");
+        break;
+        case "reprovado":
+            console.log("Sentimos muito, tente novamente no próximo processo seletivo.");
+            break;
+            default:
+                console.log("Não se aplica.");
+}
