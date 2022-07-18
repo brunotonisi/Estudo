e2226f41e8ac6e5af1cbb9efdbb1aded1b2251c2
Bruno <brunotonisi@gmail.com>
2022-07-18 10:26:25 -0300

    lista de exercicios js feita

--- scripts.js
+++ scripts.js
@@ -28,18 +28,210 @@
 
 //}
 
-let estado="teste";
+//let estado="teste";
 
-switch (estado) {
-    case "aprovado":
-        console.log("Parabens, clique aqui para fazer sua matrícula.");
-        break;
-    case "lista":
-        console.log("Enviaremos um e-mail caso haja desistência de algum candidato aprovado, aguarde.");
-        break;
-        case "reprovado":
-            console.log("Sentimos muito, tente novamente no próximo processo seletivo.");
-            break;
-            default:
-                console.log("Não se aplica.");
-}
+////switch (estado) {
+//  case "aprovado":
+//      console.log("Parabens, clique aqui para fazer sua matrícula.");
+//       break;
+//  case "lista":
+//      console.log("Enviaremos um e-mail caso haja desistência de algum candidato aprovado, aguarde.");
+//       break;
+//       case "reprovado":
+//           console.log("Sentimos muito, tente novamente no próximo processo seletivo.");
+//           break;
+//           default:
+//               console.log("Não se aplica.");
+//}
+
+// let numero=5;
+// let outro=9;
+// let aindaoutro=17;
+
+// if(numero>outro && numero>aindaoutro){
+//     console.log(numero);
+// }
+// if(outro>numero && outro>aindaoutro){
+//     console.log(outro);
+// }
+// else{
+//     console.log(aindaoutro);
+// }
+
+// let numero =5;
+
+// if (numero > 0) {
+//     console.log("positivo");
+// }
+// if (numero < 0) {
+//     console.log("negativo");
+// }
+// else {
+//     console.log("zero.");
+// }
+
+// let const1=60;
+// let const2=60;
+// let const3=62;
+// let triangulo;
+
+// if (const1<=0 || const2 <=0 || const3<=0){
+//     console.log("ângulo inválido.")
+//     }
+
+// else if(const1+const2+const3===180){
+// console.log(true);
+// }
+
+// else{
+//     console.log(false);
+// }
+
+
+
+// let peça = "balde";
+
+// switch (peça.toLowerCase()) {
+//     case "peao":
+//         console.log("uma casa para frente.");
+//         break;
+//     case "bispo":
+//         console.log("diagonal");
+//         break;
+//     case "rei":
+//         console.log("qualquer direção, uma casa por vez.");
+//         break;
+//     case "torre":
+//         console.log("linha reta, quantas casas puder.");
+//         break;
+//     case "cavalo":
+//         console.log("movimento em L");
+//         break;
+//     case "rainha":
+//         console.log("qualquer direção, quantas casas quanto possível");
+//         break;
+//         default:
+//             console.log("peça nao existente.");
+
+
+// }
+
+// let porcentagem = 50;
+
+// if (porcentagem >= 90 && porcentagem <= 100) {
+//     console.log("a");
+// }
+// if (porcentagem >= 80 && porcentagem < 90) {
+//     console.log("b");
+// }
+// if (porcentagem >= 70 && porcentagem < 80) {
+//     console.log("c");
+// }
+// if (porcentagem >= 60 && porcentagem < 70) {
+//     console.log("d");
+// }
+// if (porcentagem >= 50 && porcentagem < 60) {
+//     console.log("e");
+// }
+// if (porcentagem < 50 && porcentagem >= 0) {
+//     console.log("f");
+// }
+// else if (porcentagem < 0 || porcentagem > 100) {
+//     console.log("o valor não corresponde ao espectro determinado.");
+// }
+
+
+
+// const n1=1;
+// const n2=5;
+// const n3=9;
+
+// if(n1%2==0 || n2%2==0 || n3%2==0){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+
+//  const n1=2;
+//  const n2=6;
+//  const n3=4;
+
+//  if(n1%2!=0 || n2%2!=0 || n3%2!=0){
+//      console.log(true);
+//  }
+//  else{
+//      console.log(false);
+//  }
+
+// const val1 = 100 + (100*0.2);
+// const val2 = 200;
+// let lucro = val2-val1;
+
+
+// console.log(1000*lucro);
+
+// let salario = 3000;
+// let inss;
+// let ir;
+
+// let rinss;
+// let rir;
+// let sbase;
+
+
+
+// if (salario <= 1556.94 && salario > 0) {
+//     inss = (salario * 0.08);
+//     rinss = (salario - inss);
+// }
+// if (salario >= 1556.95 && salario <= 2594.92) {
+//     inss = (salario * 0.09);
+//     rinss = (salario - inss);
+// }
+// if (salario >= 2594.93 && salario <= 5189.82) {
+//     inss = (salario * 0.11);
+//     rinss = (salario - inss);
+// }
+// if (salario >= 5189.83) {
+//     rinss = (salario - 570.88);
+// }
+
+// if (rinss > 0 && rinss <= 1903.98) {
+//     sbase = (rinss);
+// }
+
+
+// if (rinss >= 1903.99 && rinss <= 2826.65) {
+//     rir = (rinss * 0.075) - 142.8;
+//     sbase = (rinss - rir);
+// }
+// if (rinss >= 2826.66 && rinss <= 3751.05) {
+//     rir = (rinss * 0.15)- 354.8;
+//     sbase = (rinss - rir);
+// }
+// if (rinss >= 3751.06 && rinss <= 4664.68){
+//     rir = ((rinss * 0.225)-636.13);
+//     sbase = (rinss - rir);
+// }
+// if(rinss>= 4664.69){
+//     rir = (rinss* 0.275)-869.36;
+//     sbase = (rinss - rir);
+// }
+
+
+
+// console.log(sbase);
+
+
+
+
+
+
+
+
+
+
+
+
