218a5095230d4f1a991dfd66e8c1eb2047efb134
Bruno <brunotonisi@gmail.com>
2022-08-07 08:08:08 -0300

    primeiors passos com DOM

---
+++ Administrador.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = '#FF9F84';
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "#F9DB5E";
+
+document.getElementsByClassName("emergency-tasks")[0].style.color = 'white';
+document.getElementsByClassName("no-emergency-tasks")[0].style.color = 'white';
+
+document.getElementsByTagName("h3")[0].style.backgroundColor = '#A500F3';
+document.getElementsByTagName("h3")[1].style.backgroundColor = '#A500F3';
+
+document.getElementsByTagName("h3")[2].style.backgroundColor = '#232525';
+document.getElementsByTagName("h3")[3].style.backgroundColor = '#232525';
+
+
